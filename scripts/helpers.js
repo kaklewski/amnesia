89bf11cce5d6c8f549611c5c1e64ff043907955d
Oskar Kąklewski <oskarkaklewski@gmail.com>
2025-09-03 22:54:39 +0200

    refactor: Export each helper function separately

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -1,35 +1,26 @@
-function changeElementVisibility(element, visibility) {
+export function changeElementVisibility(element, visibility) {
   element.dataset.visible = `${visibility}`;
 }
 
-function enableTransitions() {
+export function enableTransitions() {
   setTimeout(() => {
     document.getElementsByTagName('body')[0].dataset.showTransitions = 'true';
   }, 100);
 }
 
-function getCutoff(days) {
+export function getCutoff(days) {
   return Date.now() - days * 24 * 60 * 60 * 1000;
 }
 
-function getNumberOfDaysFromInput(input) {
+export function getNumberOfDaysFromInput(input) {
   return parseInt(input.value, 10);
 }
 
-function isPositiveInteger(value) {
+export function isPositiveInteger(value) {
   const number = parseInt(value, 10);
   return Number.isInteger(number) && number > 0;
 }
 
-function toKebabCase(string) {
+export function toKebabCase(string) {
   return string.replace(/([a-z0-9])([A-Z])/g, '$1-$2').toLowerCase();
 }
-
-export {
-  changeElementVisibility,
-  enableTransitions,
-  getCutoff,
-  getNumberOfDaysFromInput,
-  isPositiveInteger,
-  toKebabCase,
-};
